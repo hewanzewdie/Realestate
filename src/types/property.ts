4f28df80936d39674160f79b20ad05840d5249ad
hewanzewdie <152095332+hewanzewdie@users.noreply.github.com>
2025-09-25 20:55:24 +0300

    Update property.ts

--- src/types/property.ts
+++ src/types/property.ts
@@ -10,9 +10,6 @@ export interface Property {
   forSale: boolean;
   forRent: boolean;
   salePrice?: number;
-  bedRooms: number;
-  bathRooms: number;
-  area: number;
   rentPrice?: number;
   leaseTerm?: string;
   status: 'available' | 'sold' | 'rented' | 'pending'; 
